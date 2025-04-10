3609f03955668fe8ae71d2bbd7f07787a9c75ab6
Daisy <wanqian@ucsb.edu>
2025-04-09 21:19:18 -0700

    done with tests

--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -1,17 +1,19 @@
 package edu.ucsb.cs156.spring.hello;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class TeamTest {
 
     Team team;
+    Team team1;
 
     @BeforeEach
     public void setup() {
-        team = new Team("test-team");    
+        team = new Team("test-team");  
+        team1 = new Team("new-test");  
     }
 
     @Test
@@ -19,8 +21,33 @@ public class TeamTest {
        assert(team.getName().equals("test-team"));
     }
 
+    @Test
+    public void toString_returns_correct_string() {
+        assertEquals("Team(name=test-team, members=[])", team.toString());
+    }
+
    
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
+    @Test
+    public void testEquals_sameObject() {
+        assert(team.equals(team));
+    }
+
+    @Test
+    public void testEquals_diffClass() {
+        assert(!(team.equals(team1)));
+    }
+
+    @Test
+    public void testObject_equal(){
+        assertFalse(team.equals(1));
+    }
+
+    @Test
+    public void hash_equal(){
+        int result = team.hashCode();
+        assertEquals(-1226298695, result);
+    }
 
 }
